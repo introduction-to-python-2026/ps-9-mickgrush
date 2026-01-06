7ef54dd1d3c07908e86082167951fbb645e57c4f
mickgrush <mick.grush@gmail.com>
2026-01-06 18:44:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ scaled_X = scaler.fit_transform(X)
 
 from sklearn.model_selection import train_test_split
 
-X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.2, random_state = 42)
+X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.2, random_state= 2)
 
 from sklearn.svm import SVC
 

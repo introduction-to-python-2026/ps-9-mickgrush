6806cb95b71999cf0055af0ba1c8f52bdf9b5be0
mickgrush <mick.grush@gmail.com>
2026-01-06 18:26:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+# Download the data from your GitHub repository
+!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
+!wget https://raw.githubusercontent.com/yotam-biu/python_utils/main/lab_setup_do_not_edit.py -O /content/lab_setup_do_not_edit.py
+import lab_setup_do_not_edit
 
+import matplotlib.pyplot as plt
+import pandas as pd
+df = pd.read_csv('/content/parkinsons.csv')
+df
+
+
+X = df[['MDVP:Fo(Hz)','MDVP:Flo(Hz)']]
+y = df['status']
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler(feature_range = (0,1))
+scaled_X = scaler.fit_transform(X)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(scaled_X, y, test_size=0.2, random_state = 42)
+
+from sklearn.svm import SVC
+
+svc = SVC()
+svc.fit(X,y)
+
+y_pred = svc.predict(X_test)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
+print(accuracy)
